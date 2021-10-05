e3494e8cdcc4d5d49c244255c94fbcb10d331f6c
= <=>
2021-10-05 16:22:19 +0200

    errors

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-SERVER = 'DESKTOP-9JN77OU\MSSQLSERVER1'
-DATABASE= 'POC_DB'
-=======
 SERVER = '.'
 DATABASE= 'POC_DB'
 DRIVER= 'SQL Server Native Client 11.0'
->>>>>>> refs/remotes/origin/main
--- dbConnection.py
+++ dbConnection.py
@@ -7,11 +7,7 @@ conn = pyodbc.connect(f'Driver={config.DRIVER};'
                       'Trusted_Connection=yes;')
 
 cursor = conn.cursor()
-<<<<<<< HEAD
-cursor.execute('SELECT * FROM DATASET')
-=======
 cursor.execute('SELECT * FROM DATASET')
 
 kak = cursor.fetchall()
 print(kak)
->>>>>>> refs/remotes/origin/main
