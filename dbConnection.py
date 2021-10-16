ad941984a59eaa970a6c8d9e1f4841136fd8262c
Jens De Staercke <jens.destaercke@student.hogent.be>
2021-10-16 17:19:35 +0200

    Extra table, renames, performance increase

--- dbConnection.py
+++ dbConnection.py
@@ -13,30 +13,32 @@ conn = pyodbc.connect(f'Driver={config.DRIVER};'
 def fillDatabase():
     cursor = conn.cursor()
     
-    cursor.execute("truncate table DATASET")
-    
     data_cases_agesex(cursor)
     data_mort(cursor)
 
     cursor.commit()
 
 def data_cases_agesex(cursor):
+    cursor.execute("truncate table Cases")
+
     res = requests.get(
         'https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.json').json()
 
     for elem in res:
         print(elem)
-        cursor.execute("insert into DATASET(DATE, PROVINCE, REGION, AGEGROUP, SEX, CASES) values (?, ?, ?, ?, ?, ?)", datetime.strptime(elem['DATE'], "%Y-%m-%d") if elem.get(
+        cursor.execute("insert into Cases(DATE, PROVINCE, REGION, AGEGROUP, SEX, CASES) values (?, ?, ?, ?, ?, ?)", datetime.strptime(elem['DATE'], "%Y-%m-%d") if elem.get(
             'DATE') is not None else None, elem.get('PROVINCE', None), elem.get('REGION', None), elem.get('AGEGROUP', None), elem.get('SEX', None), elem.get('CASES', None))
 
 def data_mort(cursor):
+    cursor.execute("truncate table Mort")
+
     res = requests.get(
         'https://epistat.sciensano.be/Data/COVID19BE_MORT.json').json()
 
     for elem in res:
         print(elem)
-        cursor.execute("insert into DATASET(DATE, PROVINCE, REGION, AGEGROUP, SEX, CASES) values (?, ?, ?, ?, ?, ?)", datetime.strptime(elem['DATE'], "%Y-%m-%d") if elem.get(
-            'DATE') is not None else None, elem.get('PROVINCE', None), elem.get('REGION', None), elem.get('AGEGROUP', None), elem.get('SEX', None), elem.get('CASES', None))
+        cursor.execute("insert into Mort(DATE, REGION, AGEGROUP, SEX, DEATHS) values (?, ?, ?, ?, ?)", datetime.strptime(elem['DATE'], "%Y-%m-%d") if elem.get(
+            'DATE') is not None else None, elem.get('REGION', None), elem.get('AGEGROUP', None), elem.get('SEX', None), elem.get('DEATHS', None))
 
 
 schedule.every().day.at("01:00").do(fillDatabase)
