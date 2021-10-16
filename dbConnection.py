356cbacfb6bed5c7eba8aaef52ab0d190585f671
Jens De Staercke <jens.destaercke@student.hogent.be>
2021-10-16 15:11:00 +0200

    Tussentijdse push

--- dbConnection.py
+++ dbConnection.py
@@ -15,15 +15,23 @@ def fillDatabase():
     
     cursor.execute("truncate table DATASET")
     
+    
+
+    res = requests.get(
+        'https://epistat.sciensano.be/Data/COVID19BE_MORT.json').json()
+    
+    
+    
+    cursor.commit()
+
+def data_cases_agesex(cursor):
     res = requests.get(
         'https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.json').json()
-    
+
     for elem in res:
         print(elem)
         cursor.execute("insert into DATASET(DATE, PROVINCE, REGION, AGEGROUP, SEX, CASES) values (?, ?, ?, ?, ?, ?)", datetime.strptime(elem['DATE'], "%Y-%m-%d") if elem.get(
             'DATE') is not None else None, elem.get('PROVINCE', None), elem.get('REGION', None), elem.get('AGEGROUP', None), elem.get('SEX', None), elem.get('CASES', None))
-    
-    cursor.commit()
 
 schedule.every().day.at("01:00").do(fillDatabase)
 
