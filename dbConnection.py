dac63cd9e70adb0d5dcf94d8f62fd1025aea020e
Goddeeris Edouard <edouard.goddeeris@student.hogent.be>
2021-10-25 14:12:26 +0200

    added db initialization on startup

--- dbConnection.py
+++ dbConnection.py
@@ -10,13 +10,49 @@ conn = pyodbc.connect(f'Driver={config.DRIVER};'
                       f'Database={config.DATABASE};'
                       'Trusted_Connection=yes;')
 
+
+def init_db():
+    sql_create_cases_table = """
+    IF OBJECT_ID(N'dbo.Cases', N'U') IS NULL
+    BEGIN
+        CREATE TABLE Cases(
+        [DATE] DATE,
+        PROVINCE TEXT,
+        REGION TEXT,
+        AGEGROUP TEXT,
+        SEX VARCHAR(1),
+        CASES INT
+        )
+    END
+    """
+
+    sql_create_mort_table = """
+    IF OBJECT_ID(N'dbo.Mort', N'U') IS NULL
+    BEGIN
+        CREATE TABLE Mort(
+        [DATE] DATE,
+        REGION TEXT,
+        AGEGROUP TEXT,
+        SEX VARCHAR(1),
+        DEATHS INT
+        )
+    END"""
+
+    cursor = conn.cursor()
+    cursor.execute(sql_create_cases_table)
+    cursor.execute(sql_create_mort_table)
+    cursor.commit()
+    print("database has been initialized!")
+
+
 def fillDatabase():
     cursor = conn.cursor()
-    
+
     data_cases_agesex(cursor)
     data_mort(cursor)
 
     cursor.commit()
+
 
 def data_cases_agesex(cursor):
     cursor.execute("truncate table Cases")
@@ -28,6 +64,7 @@ def data_cases_agesex(cursor):
         print(elem)
         cursor.execute("insert into Cases(DATE, PROVINCE, REGION, AGEGROUP, SEX, CASES) values (?, ?, ?, ?, ?, ?)", datetime.strptime(elem['DATE'], "%Y-%m-%d") if elem.get(
             'DATE') is not None else None, elem.get('PROVINCE', None), elem.get('REGION', None), elem.get('AGEGROUP', None), elem.get('SEX', None), elem.get('CASES', None))
+
 
 def data_mort(cursor):
     cursor.execute("truncate table Mort")
@@ -41,10 +78,11 @@ def data_mort(cursor):
             'DATE') is not None else None, elem.get('REGION', None), elem.get('AGEGROUP', None), elem.get('SEX', None), elem.get('DEATHS', None))
 
 
+init_db()
 schedule.every().day.at("01:00").do(fillDatabase)
 
 while True:
     schedule.run_pending()
     time.sleep(60)
-    
+
 # nohup python3.8 dbConnection.py &
