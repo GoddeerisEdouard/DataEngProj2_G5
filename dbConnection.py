8045a40fd2e5aee87d9f8f29b40d1e92dcbe845b
Robbe De Kuyffer <robbe.dekuyffer@student.hogent.be>
2021-11-12 15:14:44 +0100

    Nog beter wa

--- dbConnection.py
+++ dbConnection.py
@@ -62,22 +62,36 @@ def sql_table_manipulation(table, cursor: pyodbc.Cursor, data_url, variable_list
         functions.logging(cursor, f"There was an error in retrieving {table} data")
         return
     
-    rows_affected = 0
-    for row in delete_data:
-        list_ = []
-        for item in variable_list:
-            list_.append(functions.variable_switch(item, row[item]) if item in row else None)
-        cursor.execute(functions.sql_delete_where(table, variable_list), list_)
+    def effect(data, functie):
+        rows_affected = 0
+        for iteration, row in enumerate(data):
+            list_ = []
+            for item in variable_list:
+                list_.append(functions.variable_switch(item, row[item]) if item in row else None)
+            cursor.execute(functie(table, variable_list), list_)
+            rows_affected = iteration
+        return rows_affected
+
+    rows_affected = effect(delete_data, lambda table, variable_list: functions.sql_delete_where(table, variable_list))
+    rows_affected = effect(new_data, lambda table, variable_list: functions.sql_insert_into(table, variable_list))
+    
+    # for row in delete_data:
+    #     list_ = []
+    #     for item in variable_list:
+    #         list_.append(functions.variable_switch(item, row[item]) if item in row else None)
+    #     cursor.execute(functions.sql_delete_where(table, variable_list), list_)
           
-    for iteration, row in enumerate(new_data):
-        list_ = []
-        for item in variable_list:
-            list_.append(functions.variable_switch(item, row[item]) if item in row else None)
-        cursor.execute(functions.sql_insert_into(table, variable_list), list_)
-        rows_affected = iteration
+    # for iteration, row in enumerate(new_data):
+    #     list_ = []
+    #     for item in variable_list:
+    #         list_.append(functions.variable_switch(item, row[item]) if item in row else None)
+    #     cursor.execute(functions.sql_insert_into(table, variable_list), list_)
+    #     rows_affected = iteration
     
     functions.logging(cursor, f"Table {table} filled", ra=rows_affected)
 
+
+        
 #init_db()
 #fill_database()
 schedule.every().day.at("01:00").do(fill_database)
